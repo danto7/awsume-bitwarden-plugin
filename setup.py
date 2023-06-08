671ff6ca3bd377953c72b4cfcfaa39a445876cdc
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-06-08 13:46:52 +0200

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="awsume-bitwarden-plugin",
-    version="0.0.1",
-    description="Automates awsume MFA entry via bitwarden CLI.",
-    entry_points={"awsume": ["bitwarden = bitwarden"]},
-    author="Daniel Jensen",
-    py_modules=["bitwarden"],
-)
+setup()
